a43dbd60d9a02633812d5fd0e0dadd780968df13
Donghunn-Lee <93193485+Donghunn-lee@users.noreply.github.com>
2024-12-03 18:58:02 +0900

    chore: 기본 index.tsx 초기화

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,3 @@
-import { Inter } from 'next/font/google';
-
-const inter = Inter({ subsets: ['latin'] });
-
 export default function Home() {
-  return <>
-  <div className='flex items-center'>
-djdkk
-  </div>
-  </>;
+  return <></>;
 }
